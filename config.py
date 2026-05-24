442d83009e3d18ac9a9da92c6be507fe12360112
nobitasir251-blip <nobitasir251@gmail.com>
2026-05-24 23:42:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,7 +87,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY") # Get it from http://dashboard.heroku.
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/syntaxworldcodes/VIKRANT_MUSIC",
+    "https://github.com/nobitasir251-blip/VIKRANT_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
